45645f9ea16d34bf763eb11cc7005683d3cf1393
Ajay <ajay242105@gmail.com>
2024-10-02 02:04:29 +0530

    applicatio logic

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js"; 
 import companyRoute from "./routes/company.route.js"; 
 import jobRoute from "./routes/job.route.js"; 
-
-
+import applicationRoute from "./routes/application.route.js";
 dotenv.config(); 
 const app = express(); 
 
@@ -20,6 +19,7 @@ const corsOptions = {
     origin: 'http://localhost:5173', 
     credentials: true 
 }; 
+
 app.use(cors(corsOptions)); 
 
 const PORT = process.env.PORT || 3000; // default port
@@ -28,13 +28,13 @@ const PORT = process.env.PORT || 3000; // default port
 app.use("/api/v1/user", userRoute); 
 app.use("/api/v1/company", companyRoute); 
 app.use("/api/v1/job", jobRoute); 
+app.use("/api/v1/application", applicationRoute); 
 
-
-
-// API working routes
-// "http://localhost:8000/api/v1/user/register";
-// "http://localhost:8000/api/v1/user/login";
-// "http://localhost:8000/api/v1/user/update/profile";
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
+});
 
 app.listen(PORT, () => { 
     connectDB(); 
